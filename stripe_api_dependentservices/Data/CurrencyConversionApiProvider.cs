52ff945f4e99a203a8b5efb29a2a7b9be6817294
Blair Brown <blair.j.brown@gmail.com>
2021-01-01 23:57:30 -0500

    starting setup process

---
+++ stripe_api_dependentservices/Data/CurrencyConversionApiProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace stripe_api_dependentservices.Data
+{
+    public class CurrencyConversionApiProvider
+    {
+       // make an http request and get a response back, then deserialzie the object
+    }
+}
---
+++ stripe_api_dependentservices/Services/CurrencyExchangeService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace stripe_api_dependentservices.Services
+{
+    public class CurrencyExchangeService
+    {
+
+        //make a async method that returns a Task<List<Currency>> that gets all currencies
+
+        //call into API provider class
+
+    }
+}
